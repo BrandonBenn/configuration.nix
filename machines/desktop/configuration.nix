05320efb9885850a1569473a420ee4640dfe47d2
Brandon Duval Benn <me@brandonbenn.xyz>
2023-11-15 09:12:45 +0800

    Auto-commit: Modified files - machines/desktop/configuration.nix

--- machines/desktop/configuration.nix
+++ machines/desktop/configuration.nix
@@ -24,10 +24,6 @@
   networking.hostName = "desktop"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Enable networking
   networking.networkmanager.enable = true;
   networking.extraHosts = let
@@ -90,12 +86,18 @@
         main = {
           capslock = "overload(control, esc)";
           rightalt = "layer(rightalt)";
+          fn = "layer(fn)";
         };
         rightalt = {
           h = "left";
           j = "down";
           k = "up";
           l = "right";
+        };
+        fn = {
+          "=" = "volumeup";
+          "-" = "volumedown";
+          "0" = "mute";
         };
       };
     };
