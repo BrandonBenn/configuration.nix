029b20d46e6b91961573906b40bc274811e5be68
Brandon Duval Benn <me@brandonbenn.xyz>
2023-11-16 20:56:45 +0800

    Auto-commit: Modified files - dot-config/home-manager/common.nix

--- dot-config/home-manager/common.nix
+++ dot-config/home-manager/common.nix
@@ -7,6 +7,8 @@ in
 
 {
 
+  manual.html.enable = true;
+  news.display = "silent";
   home.sessionVariables = {
     CC  = ''${pkgs.zig}/bin/zig cc'';
     CXX = ''${pkgs.zig}/bin/zig c++'';
@@ -36,7 +38,29 @@ in
         wip = "!git add -A && git commit -m 'WIP'";
         lazy = ''!git add -A && git commit -m "$(git diff --name-status)"'';
         undo = "!git reset HEAD~1 --mixed";
+        am = "!git commit --amend";
         sync = "!f() { git fetch --tags && git pull && git push; };f";
+        c = let
+          scriptPath = builtins.toFile "lazy-commit.sh" ''
+          	#!/bin/sh
+          	# Add all changes to staging area
+          	git add .
+
+          	# Get a list of modified, added, and deleted files
+          	modified_files=$(git diff --name-only --cached --diff-filter=M | awk '{printf "%s%s", sep, $1; sep=", "}')
+          	added_files=$(git diff --name-only --cached --diff-filter=A | awk '{printf "%s%s", sep, $1; sep=", "}')
+          	deleted_files=$(git diff --name-only --cached --diff-filter=D | awk '{printf "%s%s", sep, $1; sep=", "}')
+
+          	# Formulate the commit message
+          	commit_message="Auto-commit:"
+          	[ -n "$modified_files" ] && commit_message="$commit_message Modified files - $modified_files"
+          	[ -n "$added_files" ] && commit_message=", $commit_message Added files - $added_files"
+          	[ -n "$deleted_files" ] && commit_message=", $commit_message Deleted files - $deleted_files"
+          	# Commit the changes
+          	git commit -m "$commit_message"
+          	echo "Committed changes."
+          '';
+          in "!sh ${scriptPath}";
       };
     };
   };
